48246555716d1f575458892517f8697689264660
bell12 <suzuki121212@gmail.com>
2020-08-06 13:22:21 +0900

    test

--- tests/hoge/test_vending_machine.py
+++ tests/hoge/test_vending_machine.py
@@ -2,6 +2,13 @@ from vending_machine import vending_machine
 
 
 class TestVendingMachine:
+    vending_machine = VendingMachine()
+
+    def test_insert(self):
+        assert vending_machine.insert(10) == 10
+
     def test_insert(self):
         assert Huga().piyo(10) == 10
+
+
 
--- vending_machine/hoge/vending_machine.py
+++ vending_machine/hoge/vending_machine.py
@@ -9,7 +9,15 @@ class VendingMachine:
     def __init__(self):
         self.total = 0
 
-    def insert(self, add_money) -> str:
+    def insert(self, add_money) -> int:
         """insert money"""
         self.total += add_money
+        return self.total
+
+    def show_total(self) -> int:
+        """insert money"""
+        return self.total
+
+    def refund(self) -> int:
+        """insert money"""
         return self.total
