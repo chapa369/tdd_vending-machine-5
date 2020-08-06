fe0aee8aa33dde04695336d9d47aae389f617902
kaitoumetsu <umetsu.k.1111@gmail.com>
2020-08-06 14:08:03 +0900

    テスト通りました

--- tests/hoge/test_vending_machine.py
+++ tests/hoge/test_vending_machine.py
@@ -1,14 +1,11 @@
-from vending_machine import vending_machine
+from vending_machine.hoge.vending_machine import VendingMachine
 
 
 class TestVendingMachine:
-    vending_machine = VendingMachine()
-
     def test_insert(self):
+        vending_machine = VendingMachine()
         assert vending_machine.insert(10) == 10
 
-    def test_insert(self):
-        assert Huga().piyo(10) == 10
-
-
+    # def test_insert(self):
+    #   assert Huga().piyo(10) == 10
 
