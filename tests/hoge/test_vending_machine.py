1a2b89ab45e95c3f8f635696f64b583d9dd282ed
takuma-sato <68371425+takuma-sato@users.noreply.github.com>
2020-08-06 14:20:08 +0900

    [fix]func insert and show_total

--- tests/hoge/test_vending_machine.py
+++ tests/hoge/test_vending_machine.py
@@ -1,14 +1,17 @@
-from vending_machine import vending_machine
+from vending_machine.hoge.vending_machine import VendingMachine
 
 
 class TestVendingMachine:
-    vending_machine = VendingMachine()
-
     def test_insert(self):
-        assert vending_machine.insert(10) == 10
+        vm = VendingMachine()
 
-    def test_insert(self):
-        assert Huga().piyo(10) == 10
+        assert vm.insert(10) == 10
 
+    def test_show_total(self):
+        vm = VendingMachine()
 
+        add_total = vm.insert(10)
+        add_total = vm.insert(50)
+        add_total = vm.insert(50)
 
+        assert vm.show_total() == add_total
--- vending_machine/hoge/vending_machine.py
+++ vending_machine/hoge/vending_machine.py
@@ -7,17 +7,19 @@ class VendingMachine:
     """
 
     def __init__(self):
-        self.total = 0
+        self.add_total = 0
+        self.price_total = 0
 
     def insert(self, add_money) -> int:
         """insert money"""
-        self.total += add_money
-        return self.total
+        self.add_total += add_money
+        return self.add_total
 
     def show_total(self) -> int:
-        """insert money"""
-        return self.total
+        """show total money"""
+        return self.add_total
 
-    def refund(self) -> int:
-        """insert money"""
-        return self.total
+    # def refund(self) -> int:
+    #     """refund money"""
+    #     refund_total = self.add_total - self.price_total
+    #     return refund_total
