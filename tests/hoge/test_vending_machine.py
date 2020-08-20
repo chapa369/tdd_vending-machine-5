7a15337fa5eb0682b13fa2869c0d1d87150a75f1
chapa369 <hchapam@gmail.com>
2020-08-20 13:11:00 +0900

    fixed

--- tests/hoge/test_vending_machine.py
+++ tests/hoge/test_vending_machine.py
@@ -1,12 +1,14 @@
 from vending_machine.hoge.vending_machine import VendingMachine
+import pytest
 
 
 class TestVendingMachine:
-    def test_insert(self):
+    @pytest.mark.parametrize("coin,added_to_total", [(10, 10), (20, 0)])
+    def test_insert(self, coin, added_to_total):
         vm = VendingMachine()
         # 10円玉、50円玉、100円玉、500円玉、1000円札を１つずつ投入できる。
-        assert vm.insert(10) == 10
-        assert vm.insert(20) == 0
+        assert vm.insert(coin) == added_to_total
+        # assert vm.insert(coin)
 
     def test_show_total(self):
         vm = VendingMachine()
@@ -24,3 +26,5 @@ class TestVendingMachine:
             total_test += vm.insert(coin)
         assert vm.show_total() == total_test
 
+    def test_buy(self):
+        pass
