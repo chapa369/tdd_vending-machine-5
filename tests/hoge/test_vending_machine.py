3c652a34ffb0ab58f40b69180d32eb18ce4d54b8
chapa369 <hchapam@gmail.com>
2020-08-20 12:56:51 +0900

    1サイクル目終了

--- tests/hoge/test_vending_machine.py
+++ tests/hoge/test_vending_machine.py
@@ -4,14 +4,23 @@ from vending_machine.hoge.vending_machine import VendingMachine
 class TestVendingMachine:
     def test_insert(self):
         vm = VendingMachine()
-
+        # 10円玉、50円玉、100円玉、500円玉、1000円札を１つずつ投入できる。
         assert vm.insert(10) == 10
+        assert vm.insert(20) == 0
 
     def test_show_total(self):
         vm = VendingMachine()
+        insert_coin = [10, 20, 50, 50]
+        total_test = 0
+        for coin in insert_coin:
+            total_test += vm.insert(coin)
+        assert vm.show_total() == total_test
 
-        add_total = vm.insert(10)
-        add_total = vm.insert(50)
-        add_total = vm.insert(50)
+    def test_refund(self):
+        vm = VendingMachine()
+        insert_coin = [10, 20, 50, 50]
+        total_test = 0
+        for coin in insert_coin:
+            total_test += vm.insert(coin)
+        assert vm.show_total() == total_test
 
-        assert vm.show_total() == add_total
--- vending_machine/hoge/vending_machine.py
+++ vending_machine/hoge/vending_machine.py
@@ -1,25 +1,57 @@
 """sample"""
 
 
+price = {"コーラ": 120}
+
+
 class VendingMachine:
     """
     Vending Machine class
     """
 
     def __init__(self):
-        self.add_total = 0
-        self.price_total = 0
+        self.add_total = 0  # 投入金額の合計
+        self.revenue = 0  # 売り上げ金額
+        self.storage = {"コーラ": 5}
+        # self.price_total = 0
 
     def insert(self, add_money) -> int:
         """insert money"""
-        self.add_total += add_money
-        return self.add_total
+        if add_money in [10, 50, 100, 500, 100, 1000]:
+            self.add_total += add_money
+            return add_money
+        else:
+            print(add_money)
+            return 0
 
     def show_total(self) -> int:
         """show total money"""
         return self.add_total
 
-    # def refund(self) -> int:
-    #     """refund money"""
-    #     refund_total = self.add_total - self.price_total
-    #     return refund_total
+    def refund(self) -> int:
+        """refund money"""
+        refund_total = self.add_total
+        self.add_total = 0
+        return refund_total
+
+    # 全商品の在庫確認
+    def show_storage(self):
+        all_storage = []
+        for beverage in self.storage.keys():
+            all_storage.append(
+                {"name": beverage, "price": price[beverage], "store": self.storage[beverage]}
+            )
+        return all_storage
+
+    def buy(self, beverage):
+        if beverage == "コーラ":
+            if self.add_total >= Cola.price and self.storage["コーラ"] >= 1:
+                # 金額が減る
+                self.add_total -= Cola.price
+                # 在庫が減る
+                self.storage["コーラ"] -= 1
+                # 売り上げ↑
+                self.revenue += Cola.price
+            else:
+                pass
+
